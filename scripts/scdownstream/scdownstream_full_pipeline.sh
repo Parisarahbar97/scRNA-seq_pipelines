77eb32bfea56a2c62c9a7e81c181ba29bd799fdf
Parisarahbar97 <parisa.rahbar22@imperial.ac.uk>
2025-03-02 20:17:30 +0000

    scdown full pipeline

---
+++ scripts/scdownstream/scdownstream_full_pipeline.sh
@@ -0,0 +1,28 @@
+# Set a dedicated temporary directory with strict permissions
+export TMPDIR="/rds/general/user/pr422/projects/puklandmarkproject/ephemeral/Parisa_scdownstream/tmp" 
+mkdir -p "$TMPDIR"
+chmod 777 "$TMPDIR" 
+
+# Define inputs and outputs
+samplesheet="/rds/general/user/pr422/projects/puklandmarkproject/ephemeral/Parisa/converted_h5ad_files/32human_epilep_scdownstream.csv"
+out="/rds/general/user/pr422/projects/puklandmarkproject/ephemeral/Parisa_scdownstream/output_full/run1"
+config_file="/rds/general/user/pr422/projects/puklandmarkproject/ephemeral/Parisa_scdownstream/scRNA-seq_pipelines/scripts/scdownstream/test_full.config"
+
+# Run the pipeline with resume capability
+nextflow run nf-core/scdownstream \
+  -profile imperial > /rds/general/user/pr422/projects/puklandmarkproject/ephemeral/Parisa_scdownstream/output_full/run1/test.log \
+  -c "$config_file" \
+  --input "$samplesheet" \
+  --outdir "$out" \
+  -w /rds/general/user/${USER}/ephemeral/NEXTFLOW/ \
+  --ambient_removal cellbender \
+  --doublet_detection scrublet \
+  --clustering_resolutions "0.5,1.0" \
+  --integration_methods "scvi" \
+  --integration_hvgs 10000 \
+  --cluster_per_label \
+  --cluster_global \
+  --celltypist_model "Adult_Human_PrefrontalCortex" \
+  --memory_scale 1 \
+  --skip_liana true \
+  --save_intermediates false
