b4e3c2cad20d266edddf8eb94586fe4fabd59f8d
parisa rahbar <pr422@ic.ac.uk>
2025-05-12 15:51:16 +0100

    eqtl update

--- scripts/eQTL/eQTL_Nextflow_command.sh
+++ scripts/eQTL/eQTL_Nextflow_command.sh
@@ -3,7 +3,7 @@ NXF_LOG_FILE="/rds/general/user/pr422/ephemeral/NEXTFLOW_EQTL/nextflow.log"
 
 nextflow run HaglundA/eQTL_PIPELINE -r dev \
 -profile imperial \
---outdir /rds/general/user/pr422/ephemeral/NEXTFLOW_EQTL/eQTL_output \
+--outdir /rds/general/user/pr422/ephemeral/eQTL_output \
 --gds_file /rds/general/user/pr422/projects/puklandmarkproject/ephemeral/alex/sc_eQTL_runs/BONN_post_imputation_QC.gds \
 --single_cell_file /rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/parisa_eqtl/updated_seurat_with_individual_id.rds \
 --celltype_column "cell_type" \
