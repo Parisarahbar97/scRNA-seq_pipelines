122bd4b578c8cb5a79d6256a3446de7466dc0ed0
parisa rahbar <pr422@ic.ac.uk>
2025-05-15 11:38:49 +0100

    change the paths

--- scripts/scdownstream/scdown_scanvi_annotation.sh
+++ scripts/scdownstream/scdown_scanvi_annotation.sh
@@ -27,14 +27,19 @@ done
 #Verify the CSV file:
 head -n 5 "$OUTPUT_CSV"
 
+samplesheet="/rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/parisa_scdownstream/input/samplesheet_with_filtering_parameters.csv"
+out="/rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/parisa_scdownstream/output/with_scanvi_parameters"
+config_file="/rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/scRNA-seq_pipelines/scripts/scdownstream/scdown_scanvi.config"
+
+
 export NXF_LOG_FILE=/rds/general/user/pr422/ephemeral/NEXTFLOW/scdownstream.log
 
 nextflow run nf-core/scdownstream \
   -r 85a3ca5c56 \
   -profile imperial \
-  -c "/rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/scRNA-seq_pipelines/scripts/scdownstream/scdown_scanvi.config" \
-  --input "/rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/parisa_scdownstream/input/samplesheet_with_filtering_parameters.csv" \
-  --outdir "/rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/parisa_scdownstream/output/with_scanvi_parameters" \
+  -c "$config_file" \
+  --input "$samplesheet" \
+  --outdir "$out" \
   --email pr422@ic.ac.uk \
   -w /rds/general/user/pr422/ephemeral/NEXTFLOW/ \
   --ambient_removal cellbender \
