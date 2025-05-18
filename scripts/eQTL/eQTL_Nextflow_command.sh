25e2274ad2e2c1e5bb918ee943b5ddea1e2d658a
parisa rahbar <pr422@ic.ac.uk>
2025-05-18 20:19:53 +0100

    updat eqtl script

--- scripts/eQTL/eQTL_Nextflow_command.sh
+++ scripts/eQTL/eQTL_Nextflow_command.sh
@@ -1,15 +1,19 @@
 NXF_LOG_FILE="/rds/general/user/pr422/ephemeral/NEXTFLOW_EQTL/nextflow.log"
 
 
-nextflow run HaglundA/eQTL_PIPELINE -r dev \
+outdir="/rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/parisa_eqtl/eQTL_output"
+gds_file="/rds/general/user/pr422/projects/puklandmarkproject/ephemeral/alex/sc_eQTL_runs/BONN_post_imputation_QC.gds"
+single_cell_file="/rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/parisa_eqtl/matched_seuratobj/matched_seurat_demuxlet.rds"
+
+nextflow run johnsonlab-ic/sc-eQTL-pipeline \
 -profile imperial \
---outdir /rds/general/user/pr422/ephemeral/eQTL_output \
---gds_file /rds/general/user/pr422/projects/puklandmarkproject/ephemeral/alex/sc_eQTL_runs/BONN_post_imputation_QC.gds \
---single_cell_file /rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/parisa_eqtl/updated_seurat_with_individual_id.rds \
+--outdir $outdir \
+--gds_file $gds_file \
+--single_cell_file $single_cell_file \
 --celltype_column "cell_type" \
 --individual_column "Individual_ID" \
 --counts_assay "originalexp" \
 --counts_slot "counts" \
 --optimize_pcs true \
 -with-report pipeline_report.html \
---report true
+--report true 
